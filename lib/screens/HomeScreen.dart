a00ce4805631554818c8defc30629513e5b7be68
Diyorbek Sayfullaev <dearDIOR@mail.ru>
2023-11-20 00:32:36 +0500

    HW3

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -4,10 +4,12 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double buttonWidth = 70.0;
+
     return Scaffold(
       appBar: AppBar(title: Text('Home')),
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.all(10.0),
         child: Column(
           children: [
             Text('Welcome to the Home Screen', style: TextStyle(fontSize: 18.0)),
@@ -17,39 +19,55 @@ class HomeScreen extends StatelessWidget {
               children: [
                 ElevatedButton(
                   onPressed: () {
-                    Navigator.pushNamed(context, '/profile', arguments: {'userId': '123'});
+                    Navigator.pushNamed(context, '/profile', arguments: {'userId': '2110169'});
                   },
                   style: ElevatedButton.styleFrom(
-                    minimumSize: Size(150, 40), // Set the desired button size
+                    fixedSize: Size(buttonWidth, 50),
                   ),
-                  child: Text('Go to Profile'),
+                  child: Text(
+                    'Go to Profile',
+                    style: TextStyle(fontSize: 10.0),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
                 ElevatedButton(
                   onPressed: () {
                     Navigator.pushNamed(context, '/settings');
                   },
                   style: ElevatedButton.styleFrom(
-                    minimumSize: Size(150, 40), // Set the desired button size
+                    fixedSize: Size(buttonWidth, 50),
                   ),
-                  child: Text('Go to Settings'),
+                  child: Text(
+                    'Go to Settings',
+                    style: TextStyle(fontSize: 10.0),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
                 ElevatedButton(
                   onPressed: () {
                     Navigator.pushNamed(context, '/notifications');
                   },
                   style: ElevatedButton.styleFrom(
-                    minimumSize: Size(150, 40), // Set the desired button size
+                    fixedSize: Size(buttonWidth, 50),
                   ),
-                  child: Text('Go to Notifications'),
+                  child: Text(
+                    'Go to Notifications',
+                    style: TextStyle(fontSize: 10.0),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
                 ElevatedButton(
                   onPressed: () {
                     Navigator.pushNamed(context, '/about');
                   },
                   style: ElevatedButton.styleFrom(
-                    minimumSize: Size(150, 40), // Set the desired button size
+                    fixedSize: Size(buttonWidth, 50),
                   ),
-                  child: Text('Go to About'),
+                  child: Text(
+                    'Go to About',
+                    style: TextStyle(fontSize: 10.0),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
               ],
             ),
